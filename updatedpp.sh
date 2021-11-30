9faf4f83c64c523836895da6cc3fff95e631e289
brain <craigedwards@brainbox.cc>
2021-11-30 15:03:08 +0000

    automatic merge from main to vs2022

--- updatedpp.sh
+++ updatedpp.sh
@@ -81,6 +81,7 @@ git commit -m "auto update to latest DPP master branch"
 git push
 git checkout vs2022
 git pull
-git merge main
+git merge main --squash --no-commit
+git commit -am "automatic merge from main to vs2022"
 git push
 git checkout main
